0ea46bdaf964198c0f447683ef49e43a550a9c47
deep2005-code <deeptikumari8928@gmail.com>
2026-03-20 15:50:32 +0530

    Create tailwind.config.js with custom colors and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        background: '#08090E',
+        pink: '#F2A8BC',
+        green: '#50C864',
+        spotifyGreen: '#1ED760',
+      },
+      fontFamily: {
+        display: ['Playfair Display', 'serif'],
+        sans: ['DM Sans', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
