ed70e99228d5df5f85f929779d8d8e6fc1c1f739
deep2005-code <deeptikumari8928@gmail.com>
2026-03-20 15:51:51 +0530

    Add Cinnamoroll SVG component

---
+++ src/components/Sanrio/Cinnamoroll.jsx
@@ -0,0 +1,17 @@
+<svg width="52" height="52" viewBox="0 0 52 52" xmlns="http://www.w3.org/2000/svg">
+  <g fill="none" fill-rule="evenodd">
+    <circle cx="26" cy="26" r="26" fill="#FFF"/>
+    <ellipse cx="26" cy="20" rx="20" ry="18" fill="#FFF"/>
+    <circle cx="19" cy="18" r="3" fill="#000"/>
+    <circle cx="33" cy="18" r="3" fill="#000"/>
+    <ellipse cx="26" cy="25" rx="5" ry="3" fill="#FFC0CB"/>
+    <ellipse cx="26" cy="28" width="8" height="4" fill="#FFC0CB"/>
+    <polygon points="26,28 24,25 28,25" fill="#FFC0CB"/>
+    <path d="M14,29c-2-2 0-6 3-6h20c3,0 5,4 3,6" stroke="#000" stroke-width="2" fill="none"/>
+    <path d="M18,36c4-2 8-2 12,0" stroke="#000" stroke-width="2" fill="none"/>
+    <path d="M26,36c0-1 1-2 2-2s2,1 2,2-1.5,2-2,2-2-2-2-2z" fill="#D2B48C"/>
+    <path d="M26,16c-3,0-5-2-5-4s2-4 5-4 5,2 5,4-2,4-5,4z" fill="none" stroke="#000" stroke-width="0.5"/>
+    <path d="M8,25c3-5 8-6 12-1s3,7 1,12c-3-6-8-8-12-8-2,0-4,1-4,2s0,1 0,1" fill="#D2B48C"/>
+    <path d="M44,25c-3-5-8-6-12-1s-3,7-1,12c3-6,8-8,12-8 2,0,4,1,4,2s0,1 0,1" fill="#D2B48C"/>
+  </g>
+</svg>
